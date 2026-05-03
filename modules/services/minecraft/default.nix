78ff509362be43ba8d50dad9b336c31c1d79b632
Andrew Gazelka <andrew.gazelka@gmail.com>
2026-05-02 23:05:58 -0700

    switch to GraalVM CE and use Aikar's optimized G1GC flags

--- modules/services/minecraft/default.nix
+++ modules/services/minecraft/default.nix
@@ -61,8 +61,7 @@ in
 
     javaPackage = mkOption {
       type = types.package;
-      default = pkgs.temurin-jre-bin-25;
-      description = "Java runtime used to launch the server.";
+      default = pkgs.graalvmPackages.graalvm-ce;
     };
 
     jvmFlags = mkOption {
@@ -73,8 +72,19 @@ in
         "-XX:MaxGCPauseMillis=200"
         "-XX:+DisableExplicitGC"
         "-XX:+AlwaysPreTouch"
+        "-XX:G1NewSizePercent=30"
+        "-XX:G1MaxNewSizePercent=40"
+        "-XX:G1HeapRegionSize=8M"
+        "-XX:G1ReservePercent=20"
+        "-XX:G1MixedGCCountTarget=4"
         "-XX:InitiatingHeapOccupancyPercent=15"
+        "-XX:G1MixedGCLiveThresholdPercent=90"
+        "-XX:G1RSetUpdatingPauseTimePercent=5"
+        "-XX:SurvivorRatio=32"
         "-XX:+PerfDisableSharedMem"
+        "-XX:MaxTenuringThreshold=1"
+        "-Dusing.aikars.flags=https://mcflags.emc.gs"
+        "-Daikars.new.flags=true"
       ];
       description = "JVM flags used after heap sizing and before -jar.";
     };
